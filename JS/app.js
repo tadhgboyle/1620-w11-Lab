42a529c83c1f309383cc3f521734cbc6993b35bd
Tadhg <tadhgsmboyle@gmail.com>
2021-03-23 15:31:28 -0700

    :tada: Finish week 11 lab

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,31 @@
+const todoList = [
+    {
+        todo: 'clone repo for starter code',
+        status: 'complete'
+    },
+    {
+        todo: 'set-url to new origin repo',
+        status: 'complete'
+    },
+    {
+        todo: 'add js and test',
+        status: 'started'
+    },
+    {
+        todo: 'do well in 1620',
+        status: 'started'
+    }
+];
 
+const completedTodos = [];
+
+for (const todo of todoList) {
+    if (todo.status == 'complete') {
+        completedTodos.push(todo)
+    }
+}
+
+for (const todoObj of completedTodos) {
+    ({ todo } = todoObj)
+    console.log(todo)
+}
